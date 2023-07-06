ac50ed091af7137e646744b8141b36bc659ba40b
peterywteng <peterywteng@gmail.com>
2023-07-06 21:16:29 +0800

    Create ex1-1a.cpp

---
+++ ex1-1a.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(void){
+
+    int i=1,a,b;
+    printf("i=%d a=%d b=%d \n",i,a,b ); // +,-,*,/
+    float f = 80.5;
+    char  c = 'A';
+    a = 20 ;
+    b = 50 ;
+    printf("hello Selena");
+    printf("my height is %d m %f cm \n",i,f);
+    printf("in my last name, its first letter is %c \n", c);
+    printf("a + b = %d \n",a+b ); // +,-,*,/
+    printf("%d + %d = %d \n",a,b,a+b ); // +,-,*,/,mod
+    printf("4 %% 5 = %d \n",4%5 ); // +,-,*,/,mod    
+    
+    int x, y;
+    printf("A=> ");
+    scanf("%d",&x);
+    printf("B=> ");
+    scanf("%d", &y);
+    printf("A+B=%d\n", x+y);
+    printf("A-B=%d\n", x-y);
+    
+    printf("A*B=%d\n", x*y);
+    printf("A/B=%d\n", x/y);
+    printf("A %% B=%d\n", x%y);
+    
+    
+    
+    
+    
+}
